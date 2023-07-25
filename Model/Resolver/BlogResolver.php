89f76505e4e035ef979108fefdb0642b2595d0d0
Mark Shust <mark@shust.com>
2023-07-25 12:47:00 -0400

    Create a Custom Query Resolver

---
+++ Model/Resolver/BlogResolver.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace Macademy\GraphQl\Model\Resolver;
+
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+
+class BlogResolver implements ResolverInterface
+{
+    public function resolve(
+        Field $field,
+        $context,
+        ResolveInfo $info,
+        array $value = null,
+        array $args = null
+    ): array
+    {
+        return [
+            'title' => 'My Awesome Blog',
+        ];
+    }
+}
